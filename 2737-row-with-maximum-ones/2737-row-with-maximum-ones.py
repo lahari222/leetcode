ddcbf2cc913b0ee104cd8ed4a17ca5005b6c6bfe
lahari222 <98799870+lahari222@users.noreply.github.com>
2025-05-22 21:39:32 +0530

    Time: 30 ms (10.72%), Space: 18.4 MB (88.78%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
@@ -1,18 +1,15 @@
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        new_dict={}
+        max_val=float('-inf')
+        index=0
         for i in range(0,len(mat)):
             count=0
             for j in range(0,len(mat[0])):
                 if mat[i][j]==1:
                     count+=1
-            new_dict[i]=count
-        max_val=float('-inf')
-        index=0
-        for k,v in new_dict.items():
-            if v>max_val:
-                max_val=max(max_val,v)
-                index=k
+            if count>max_val:
+                max_val=max(max_val,count)
+                index=i
         return [index,max_val]
 
         
