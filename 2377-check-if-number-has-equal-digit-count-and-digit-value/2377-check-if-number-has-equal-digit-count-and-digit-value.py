d57150a4260046359619e5e969d53c98fcfca530
lahari222 <98799870+lahari222@users.noreply.github.com>
2025-05-30 22:36:47 +0530

    Time: 0 ms (100%), Space: 17.7 MB (85.86%) - LeetHub

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.py
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.py
@@ -8,7 +8,7 @@ class Solution:
                 new_dict[i]=1
         for i in range(0,len(num)):
             expected = int(num[i])
-            actual = new_dict.get(str(i), 0)  # use str(i) for key
+            actual = new_dict.get(str(i), 0)
             if expected != actual:
                 return False
         return True
